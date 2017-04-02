c956d16c9e0067e3ec30451d44281951ce7ec75f
Schidu Vasile <sckeedoo@gmail.com>
2017-04-02 23:36:29 +0300

    Commit

--- Thread1/src/com/company/Main.java
+++ Thread1/src/com/company/Main.java
@@ -1,8 +1,36 @@
 package com.company;
 
+
+class Runner extends Thread {
+
+    @Override
+
+    public void run() {
+        for (int i = 0 ; i < 10 ; i++) {
+            System.out.println("Halo " + i);
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Runner runner1 = new Runner();
+        runner1.start();
+
+        Runner runner2 = new Runner();
+        runner2.start();
+
     }
 }
